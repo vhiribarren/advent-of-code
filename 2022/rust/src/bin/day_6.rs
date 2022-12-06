83acaf298c6373ad1c2372a5fb06cfa1f7a8e8ec
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2022-12-06 22:12:10 +0100

    Some code cleaning

--- 2022/rust/src/bin/day_6.rs
+++ 2022/rust/src/bin/day_6.rs
@@ -8,8 +8,7 @@ const SRC_PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/", file!());
 
 fn look_for_n_distinct_chars(n: usize, message: &[char]) -> usize {
     for (idx, window) in message.windows(n).enumerate() {
-        let set = window.iter().collect::<HashSet<_>>();
-        if set.len() == n {
+        if HashSet::<&char>::from_iter(window).len() == n {
             return idx + n;
         }
     }
@@ -23,10 +22,10 @@ fn main() {
     let chars = input.chars().collect::<Vec<_>>();
     println!(
         "Position for 4 chars is {}",
-        look_for_n_distinct_chars(4, chars.as_slice())
+        look_for_n_distinct_chars(4, &chars)
     );
     println!(
         "Position for 14 chars is {}",
-        look_for_n_distinct_chars(14, chars.as_slice())
+        look_for_n_distinct_chars(14, &chars)
     );
 }
