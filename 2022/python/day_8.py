c8f740140d1fcac177672c378d2f3938f97d8eac
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2022-12-08 19:09:52 +0100

    Attempt to clean and factorize some code

--- 2022/python/day_8.py
+++ 2022/python/day_8.py
@@ -5,59 +5,51 @@ import os
 INPUT_FILEPATH = os.path.join(os.path.dirname(__file__), "../inputs/day_8.txt")
 
 
-
 def main():
 
     with open(INPUT_FILEPATH) as input:
         grid = [[int(c) for c in line.strip()] for line in input]
+    x_len, y_len = len(grid[0]), len(grid)
+    x_range, y_range = range (0, x_len), range (0, y_len)
 
     visible_trees = set()
-    for y in range(0, len(grid)):
-        min_height = -1
-        for x in range (0, len(grid[0])):
-            if (new_height := grid[y][x]) > min_height:
-                min_height = new_height
-                visible_trees.add((x, y))
-
-        min_height = -1
-        for x in reversed(range (0, len(grid[0]))):
-            if (new_height := grid[y][x]) > min_height:
-                min_height = new_height
-                visible_trees.add((x, y))
-    for x in range(0, len(grid[0])):
-        min_height = -1
-        for y in range (0, len(grid)):
-            if (new_height := grid[y][x]) > min_height:
-                min_height = new_height
-                visible_trees.add((x, y))
-
-        min_height = -1
-        for y in reversed(range (0, len(grid))):
-            if (new_height := grid[y][x]) > min_height:
-                min_height = new_height
-                visible_trees.add((x, y))
-    print(len(visible_trees))
+    def check_and_add_tree():
+        nonlocal min_height
+        if (new_height := grid[y][x]) > min_height:
+            min_height = new_height
+            visible_trees.add((x, y))
+    for y in y_range:
+        for x_scan_pos in [x_range, reversed(x_range)]:
+            min_height = -1
+            for x in x_scan_pos:
+                check_and_add_tree()
+    for x in x_range:
+        for y_scan_pos in [y_range, reversed(y_range)]:        
+            min_height = -1
+            for y in y_scan_pos:
+                check_and_add_tree()
+    print("Visible trees:", len(visible_trees))
 
     scenic_scores = []
-    for y in range(0, len(grid)):
-        for x in range(0, len(grid[0])):
+    for y in y_range:
+        for x in x_range:
             scenic_score = 1
             for direction in [(0, 1), (0, -1), (-1, 0), (1, 0)]:
                 trees = 0
-                idx_x = x
-                idx_y = y
-                min_height = grid[y][x]
+                idx_x, idx_y = x, y
+                base_height = grid[y][x]
                 while True:
                     idx_x += direction[0]
                     idx_y += direction[1]
-                    if  idx_x < 0 or idx_x >= len(grid[0]) or idx_y < 0 or idx_y >= len(grid):
+                    if  idx_x < 0 or idx_x >= x_len or idx_y < 0 or idx_y >= y_len:
                         break
                     trees += 1
-                    if (new_height := grid[idx_y][idx_x]) >= min_height:
+                    if grid[idx_y][idx_x] >= base_height:
                         break
                 scenic_score *= trees
             scenic_scores.append(scenic_score)
-    print(max(scenic_scores))
+    print("Scenic score:", max(scenic_scores))
+
 
 if __name__ == "__main__":
     main()
