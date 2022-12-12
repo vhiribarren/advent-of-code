cf5595f626cba9723037baddd327136101eb1ca0
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2022-12-12 19:12:23 +0100

    Day 12, Python, full unoptimized solution

--- 2022/python/day_12.py
+++ 2022/python/day_12.py
@@ -13,18 +13,10 @@ def get_height(height: str):
         case "E": return ord("z")
         case val: return ord(val)
 
-def main():
 
-    with open(INPUT_FILEPATH) as file_content:
-        grid = [list(s) for s in [l.strip() for l in file_content]]
+def compute_paths(grid, start_coord, end_coord):
+
     ylen, xlen = len(grid), len(grid[0])
-    for y in range(ylen):
-        for x in range(xlen):
-            if grid[y][x] == "S":
-                start_coord = (x, y)
-            if grid[y][x] == "E":
-                end_coord = (x, y)
-
     prev_coords = {start_coord: None}
     dist_coords = {start_coord: 0}
     unvisited_coords = {start_coord}
@@ -54,16 +46,46 @@ def main():
                         prev_coords[candidate] = (x, y)
                         dist_coords[candidate] = new_distance
         visited_coords.add((x, y))
-
+    
+    if end_coord not in prev_coords:
+        return math.inf
     chain = []
-    endpos = end_coord
-    while endpos is not None:
-        chain.append(endpos)
-        endpos = prev_coords[endpos]
-    print(len(list(reversed(chain)))-1)
+    last_pos = end_coord
+    while last_pos is not None:
+        chain.append(last_pos)
+        last_pos = prev_coords[last_pos]
+
+    return len(list(reversed(chain)))-1
 
 
 
+def main():
+
+    with open(INPUT_FILEPATH) as file_content:
+        grid = [list(s) for s in [l.strip() for l in file_content]]
+
+    ylen, xlen = len(grid), len(grid[0])
+    for y in range(ylen):
+        for x in range(xlen):
+            if grid[y][x] == "S":
+                start_coord = (x, y)
+            if grid[y][x] == "E":
+                end_coord = (x, y)
+
+    print("Best signal steps:", compute_paths(grid, start_coord, end_coord))
+
+    a_positions = []
+    for y in range(ylen):
+        for x in range(xlen):
+            if grid[y][x] == "a" or grid[y][x] == "S":
+                a_positions.append((x, y))
+    min_steps = math.inf
+    for idx, a_position in enumerate(a_positions):
+        print(len(a_positions)-idx)
+        steps = compute_paths(grid, a_position, end_coord)
+        if steps < min_steps:
+            min_steps = steps
+    print("Minimal steps from a:", min_steps)
 
 if __name__ == "__main__":
     main()
