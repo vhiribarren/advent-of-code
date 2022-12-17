d6302598759e51ff97e14f7905c62a676ece89f6
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2022-12-17 10:48:44 +0100

    Made it... day 15, python, part 2

--- 2022/python/day_15.py
+++ 2022/python/day_15.py
@@ -4,6 +4,7 @@ import os
 import re
 import math
 from dataclasses import dataclass
+from typing import Optional
 
 
 INPUT_FILEPATH = os.path.join(os.path.dirname(__file__), "../inputs/day_15.txt")
@@ -19,6 +20,50 @@ class Triple:
 
 def manhattan_dist(l: complex, r: complex) -> int:
     return int(abs(l.real - r.real)+abs(l.imag - r.imag))
+
+
+def range_for_triple(t: Triple, *, x:int = None, y: int=None) -> Optional[list[int]]:
+    if x is not None:
+        delta_x = abs(t.sensor.real-x)
+        if delta_x > t.dist:
+            return None
+        delta_y = t.dist - delta_x
+        return [t.sensor.imag-delta_y, t.sensor.imag+delta_y]
+    if y is not None:
+        delta_y = abs(t.sensor.imag-y)
+        if delta_y > t.dist:
+            return None
+        delta_x = t.dist - delta_y
+        return [t.sensor.real-delta_x, t.sensor.real+delta_x]
+
+
+def scan_all(triples: list[Triple], min: int, max: int) -> complex:
+    for y in range(min, max+1):
+        if y % 1000 == 0:
+            print("Scanning line", y)
+        result = scan_line(triples, y, min, max)
+        if len(result) == 1:
+            return result[0]
+
+
+def scan_line(triples: list[Triple], y: int, min_col: int, max_col: int) -> list[complex]:
+    uncovered_points = []
+    covered_ranges = []
+    for t in triples:
+        if (r := range_for_triple(t, y=y)) is not None:
+            covered_ranges.append(r)
+    x = min_col
+    while True:
+        if x > max_col:
+            break
+        for l, r in covered_ranges:
+            if l <= x <= r:
+                x = r+1
+                break
+        else:
+            uncovered_points.append(complex(x, y))
+            x += 1
+    return uncovered_points
 
 
 def main():
@@ -43,20 +88,16 @@ def main():
             min_x = int(c)
 
     y = 2000000
-    no_beacon_count = 0
-    for x in range(min_x, max_x+1):
-        scan_coord = complex(x, y)
-        for t in triples:
-            if manhattan_dist(scan_coord, t.sensor) <= t.dist:
-                no_beacon_count += 1
-                break
+    no_beacon_count = max_x - min_x + 1 - len(scan_line(triples, y, min_x, max_x))
     found_beacons = set()
     for t in triples:
         if t.beacon.imag == y:
             found_beacons.add(t.beacon)
     no_beacon_count -= len(found_beacons)
-    print(no_beacon_count)
+    print("No beacon count: ", no_beacon_count)
 
+    mysterious_beacon = scan_all(triples, 0, 4000000)
+    print("Tuning frequency:", int(mysterious_beacon.real*4000000 + mysterious_beacon.imag))
 
 
 if __name__ == "__main__":
