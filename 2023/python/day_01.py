9b704e3d2959b5c548e320b03ac3f5150691a8fe
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2023-12-02 09:26:19 +0100

    Fix internal organization of files

--- 2023/python/day_01.py
+++ 2023/python/day_01.py
@@ -2,7 +2,7 @@
 
 import os
 
-INPUT_FILE = "day_1.txt"
+INPUT_FILE = "day_01.txt"
 INPUT_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "inputs", INPUT_FILE)
 
 DIGIT_MAP = {
@@ -45,5 +45,5 @@ def part_2(input: str):
 if __name__ == "__main__":
     with open(INPUT_FILEPATH) as input:
         all_input = input.read()
-        #part_1(all_input)
+        part_1(all_input)
         part_2(all_input)
