dcab5ab83999a311e6271a444504b0291c9e7769
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2024-12-01 08:03:51 +0100

    2024, Python, day 1

--- 2024/python/day_01.py
+++ 2024/python/day_01.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python3
 
-import os
+import os.path as op
+from collections import Counter
 
-INPUT_FILE = "day_01.txt"
-INPUT_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "inputs", INPUT_FILE)
+INPUT_FILEPATH = op.join(op.dirname(__file__), "..", "inputs", f"{op.splitext(op.basename(__file__))[0]}.txt")
 
+
+def parse_input(input: str) -> tuple[list[int], list[int]]:
+    list_left = []
+    list_right = []
+    for line in input.splitlines():
+        left, right = map(int, line.split())
+        list_left.append(left)
+        list_right.append(right)
+    return list_left, list_right
 
 def part_1(input: str):
-    ...
+    all_left, all_right = parse_input(input)
+    diffs = [abs(l-r) for l, r in zip(sorted(all_left), sorted(all_right))]
+    print("Part 1:", sum(diffs))
 
 def part_2(input: str):
-    ...
+    all_left, all_right = parse_input(input)
+    right_occurences = Counter(all_right)
+    similarities = [v * right_occurences[v] for v in all_left]
+    print("Part 2:", sum(similarities))
 
 
 if __name__ == "__main__":
