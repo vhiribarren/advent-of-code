b880ddd27afa5ca3128aacc1677d840b21aaaf05
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2024-12-02 07:29:00 +0100

    Compact latest problem in one function

--- 2024/python/day_02.py
+++ 2024/python/day_02.py
@@ -6,23 +6,12 @@ from itertools import pairwise
 INPUT_FILEPATH = op.join(op.dirname(__file__), "..", "inputs", f"{op.splitext(op.basename(__file__))[0]}.txt")
 
 
-def part_1(input: str):
-    reports = [map(int, l.split()) for l in input.splitlines()]
-    count = 0
-    for report in reports:
-        diffs = [l2 - l1 for l1, l2 in pairwise(report)]
-        if all(1 <= abs(d) <= 3 for d in diffs)\
-              and (all(d > 0 for d in diffs)\
-              or all(d < 0 for d in diffs)):
-            count += 1
-    print("Part 1:", count)
-
-
-def part_2(input: str):
+def solver(input: str, tolerate: bool = False) -> int :
     count = 0
     for line in input.splitlines():
         report_init = [int(l) for l in line.split()]
-        for idx in range(0, len(report_init)):
+        scanner = range(0, len(report_init)) if tolerate else [len(report_init)]
+        for idx in scanner:
             report_candidate = report_init[0:idx] + report_init[idx+1:]
             diffs = [l2 - l1 for l1, l2 in pairwise(report_candidate)]
             if all(1 <= abs(d) <= 3 for d in diffs)\
@@ -30,11 +19,10 @@ def part_2(input: str):
                   or all(d < 0 for d in diffs)):
                 count += 1
                 break
-    print("Part 2:", count)
-
+    return count
 
 if __name__ == "__main__":
     with open(INPUT_FILEPATH) as input:
         all_input = input.read()
-        part_1(all_input)
-        part_2(all_input)
+        print("Part 1", solver(all_input, tolerate=False))
+        print("Part 2", solver(all_input, tolerate=True))
