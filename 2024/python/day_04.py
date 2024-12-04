0deb06c394407474095f61fe6c83df9e57238d03
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2024-12-04 21:17:42 +0100

    Managed to simplify a little bit some code

--- 2024/python/day_04.py
+++ 2024/python/day_04.py
@@ -5,26 +5,21 @@ from pathlib import Path
 INPUT_FILEPATH = (p := Path(__file__)).parent/".."/"inputs"/f"{p.stem}.txt"
 
 
-# Quite complex, no time to do simpler for now
 def part_1(input: str):
     puzzle = input.splitlines()
     width = len(puzzle)
+    to_be_reverted_lines = []
+    for i in range(0, width):
+        to_be_reverted_lines.append("".join(puzzle[j][i] for j in range(0, width)))
+        to_be_reverted_lines.append("".join(puzzle[i+j][j] for  j in range(0, width - i)))
+        to_be_reverted_lines.append("".join(puzzle[i+j][width-1-j] for j in range(0, width - i)))
+        if i != 0:
+            to_be_reverted_lines.append("".join(puzzle[j][i+j] for  j in range(0, width - i)))
+            to_be_reverted_lines.append("".join(puzzle[j][width-1-j-i] for  j in range(0, width - i)))
     words = puzzle[:]
     words.extend([line[::-1] for line in puzzle])
-    vertical_words = []
-    for i in range(0, width):
-        vertical_words.append("".join(puzzle[j][i] for j in range(0, width)))
-    words.extend(vertical_words[:])
-    words.extend([line[::-1] for line in vertical_words])
-    diag_words = []
-    for i in range(0, width):
-        diag_words.append("".join(puzzle[i+j][j] for  j in range(0, width - i)))
-        diag_words.append("".join(puzzle[i+j][width-1-j] for j in range(0, width - i)))
-        if i != 0:
-            diag_words.append("".join(puzzle[j][i+j] for  j in range(0, width - i)))
-            diag_words.append("".join(puzzle[j][width-1-j-i] for  j in range(0, width - i)))
-    words.extend(diag_words[:])
-    words.extend([line[::-1] for line in diag_words])
+    words.extend(to_be_reverted_lines[:])
+    words.extend([line[::-1] for line in to_be_reverted_lines])
     print("Part 1:", sum([w.count("XMAS") for w in words]))
 
 
