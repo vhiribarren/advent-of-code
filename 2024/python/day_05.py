a4aa3190790c7833c56e353b44bffa63678cbe08
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2024-12-05 08:08:16 +0100

    Regrouped some statements to simplify code

--- 2024/python/day_05.py
+++ 2024/python/day_05.py
@@ -7,7 +7,6 @@ from collections import defaultdict
 
 INPUT_FILEPATH = (p := Path(__file__)).parent/".."/"inputs"/f"{p.stem}.txt"
 
-
 @dataclass
 class Either:
     left: set[str] = field(default_factory=set)
@@ -22,27 +21,23 @@ def solver(input: str):
         rules[l].right.add(r)
         rules[r].left.add(l)
 
+    def sort_func(x, y):
+        return 1 if x in rules[y].left else -1
+
     sum_correct = 0
-    incorrect_updates = []
+    sum_incorrect = 0
     for raw_update in raw_updates.splitlines():
         update = raw_update.split(",")
         for idx, page in enumerate(update):
             if set(update[:idx]) <= rules[page].left and set(update[idx+1:]) <= rules[page].right:
                 continue
-            incorrect_updates.append(update)
+            sorted_update = sorted(update, key=cmp_to_key(sort_func))
+            sum_incorrect += int(sorted_update[len(sorted_update)//2])
             break
         else:
             sum_correct += int(update[len(update)//2])
     print("Part 1:", sum_correct)
-
-    sum_incorrect = 0
-    def sort_func(x, y):
-        return 1 if x in rules[y].left else -1
-    for update in incorrect_updates:
-        sorted_update = sorted(update, key=cmp_to_key(sort_func))
-        sum_incorrect += int(sorted_update[len(sorted_update)//2])
     print("Part 2:", sum_incorrect)
-
 
 
 if __name__ == "__main__":
