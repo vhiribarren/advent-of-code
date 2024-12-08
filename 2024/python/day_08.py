65d4a15cc98b6b83e58821fd8fc5753c50035c81
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2024-12-08 07:59:06 +0100

    2024, Python, day 8

---
+++ 2024/python/day_08.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+from collections import defaultdict
+from itertools import product, count
+
+INPUT_FILEPATH = (p := Path(__file__)).parent/".."/"inputs"/f"{p.stem}.txt"
+
+
+def parser(input: str) -> tuple[dict[str, list[complex]], int, int]:
+    result = defaultdict(list)
+    for y, line in enumerate(input.splitlines()):
+        for x, c in enumerate(line):
+            if c != ".":
+                result[c].append(complex(x, y))
+    return result, x+1, y+1
+
+
+def valid_positions(position, width, height) -> bool:
+    return 0 <= position.real < width and 0 <= position.imag < height
+
+def part_1(input: str):
+    antenna, width, height = parser(input)
+    antinodes: set[complex] = set()
+    for freq in antenna.keys():
+        for l, r in product(antenna[freq], repeat=2):
+            if l == r:
+                continue
+            dir = l - r
+            for candidate in [l + dir, r - dir]:
+                if valid_positions(candidate, width, height):
+                    antinodes.add(candidate)
+    print("Part 1:", len(antinodes))
+
+def part_2(input: str):
+    antenna, width, height = parser(input)
+    antinodes = set()
+    for freq in antenna.keys():
+        for l, r in product(antenna[freq], repeat=2):
+            if l == r:
+                continue
+            dir = l - r
+            for candidate in count(l, dir):
+                if not valid_positions(candidate, width, height):
+                    break
+                antinodes.add(candidate)
+            for candidate in count(r, -dir):
+                if not valid_positions(candidate, width, height):
+                    break
+                antinodes.add(candidate)      
+    print("Part 2:", len(antinodes))
+
+
+if __name__ == "__main__":
+    with open(INPUT_FILEPATH) as input:
+        all_input = input.read()
+        part_1(all_input)
+        part_2(all_input)
