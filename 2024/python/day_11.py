8dc86687bcd049fd07b96c62c3a29680e9f6a9da
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2024-12-11 07:57:19 +0100

    A bit of linting

--- 2024/python/day_11.py
+++ 2024/python/day_11.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-from pathlib import Path
 from functools import cache
+from pathlib import Path
 
 INPUT_FILEPATH = (p := Path(__file__)).parent/".."/"inputs"/f"{p.stem}.txt"
 
@@ -10,10 +10,9 @@ INPUT_FILEPATH = (p := Path(__file__)).parent/".."/"inputs"/f"{p.stem}.txt"
 def convert_stone(stone: int) -> tuple[int, ...]:
         if stone == 0:
             return (1,)
-        elif (stone_len := len(stone_str := str(stone))) % 2 == 0:
+        if (stone_len := len(stone_str := str(stone))) % 2 == 0:
             return(int(stone_str[0:stone_len//2]), int(stone_str[stone_len//2:]))
-        else:
-            return (stone * 2024,)
+        return (stone * 2024,)
 
 @cache
 def compute_len(stone: int, iter_nb: int) -> int:
@@ -21,13 +20,13 @@ def compute_len(stone: int, iter_nb: int) -> int:
         return 1
     return sum(compute_len(new_stone, iter_nb -1) for new_stone in convert_stone(stone))
 
-def solver(input: str, iter_nb: int) -> int:
-    init = list(map(int, input.split()))
+def solver(input_data: str, iter_nb: int) -> int:
+    init = list(map(int, input_data.split()))
     return sum(compute_len(stone, iter_nb) for stone in init)
 
 
 if __name__ == "__main__":
-    with open(INPUT_FILEPATH) as input:
-        all_input = input.read()
-        print("Part 1:", solver(all_input, 25))
-        print("Part 2:", solver(all_input, 75))
+    with INPUT_FILEPATH.open() as file:
+        file_data = file.read()
+        print("Part 1:", solver(file_data, 25))
+        print("Part 2:", solver(file_data, 75))
