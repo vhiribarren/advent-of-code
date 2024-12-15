b353c36ac058351acde3f557818140fb7097169a
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2024-12-15 09:22:35 +0100

    2024, Python, day 15 part 2

--- 2024/python/day_15.py
+++ 2024/python/day_15.py
@@ -1,58 +1,120 @@
 #!/usr/bin/env python3
 
 from pathlib import Path
-from enum import Enum, auto
+from dataclasses import dataclass
 
 INPUT_FILEPATH = (p := Path(__file__)).parent/".."/"inputs"/f"{p.stem}.txt"
 
-class ElemType(Enum):
-    Box = auto()
-    Wall = auto()
+class Wall:
+    pass
 
+@dataclass(unsafe_hash=True) # We will not mutate the object while being in a set
+class Box:
+    left: complex = 0
+    right: complex = 0
+
+type GridElem = Wall | Box
+
+class BreakOuterLoop(Exception):
+    pass
+
+
+def parse_instructions(inst_data: str) -> list[complex]:
+    return [
+        { ">": 1, "<": -1, "^": -1j, "v": 1j}[c] for c in "".join(inst_data.splitlines())
+    ]
 
 def part_1(input_data: str):
     grid_raw, inst_raw = input_data.split("\n\n")
-    instructions = []
-    for c in inst_raw.strip():
-        match c:
-            case ">": instructions.append(1)
-            case "<": instructions.append(-1)
-            case "^": instructions.append(-1j)
-            case "v": instructions.append(1j)
-    grid = {}
+    instructions = parse_instructions(inst_raw)
+    grid = dict[complex, GridElem]()
     robot = None
     for y, line in enumerate(grid_raw.splitlines()):
         for x, c in enumerate(line):
             match c:
-                case "#": grid[complex(x, y)] = ElemType.Wall
-                case "O": grid[complex(x, y)] = ElemType.Box
+                case "#": grid[complex(x, y)] = Wall()
+                case "O": grid[complex(x, y)] = Box()
                 case "@": robot = complex(x, y)
+    assert robot is not None
     for inst in instructions:
         scan_pos = robot
-        scanned_elems = []
+        scanned_coords = []
         while True:
             scan_pos += inst
             match grid.get(scan_pos):
-                case ElemType.Wall:
+                case Wall():
                     break
-                case ElemType.Box:
-                    scanned_elems.append(scan_pos)
+                case Box():
+                    scanned_coords.append(scan_pos)
                 case None:
                     robot += inst
-                    for elem in reversed(scanned_elems):
-                        del grid[elem]
-                        grid[elem + inst] = ElemType.Box
+                    for scanned_coord in reversed(scanned_coords):
+                        del grid[scanned_coord]
+                        grid[scanned_coord + inst] = Box()
                     break
     gps_list = []
     for coord, elem in grid.items():
-        if elem == ElemType.Box:
+        if isinstance(elem, Box):
             gps_list.append(coord.real + 100*coord.imag)
     print("Part 1:", int(sum(gps_list)))
 
 
-
 def part_2(input_data: str):
-    ...
+    grid_raw, inst_raw = input_data.split("\n\n")
+    instructions = parse_instructions(inst_raw)
+    grid = dict[complex, GridElem]()
+    robot = None
+    boxes = list[Box]()
+    for y, line in enumerate(grid_raw.splitlines()):
+        for x, c in enumerate(line):
+            match c:
+                case "#":
+                    grid[complex(2*x, y)] = Wall()
+                    grid[complex(2*x+1, y)] = Wall()
+                case "O":
+                    box = Box(complex(2*x, y), complex(2*x+1, y))
+                    grid[box.left] = box
+                    grid[box.right] = box
+                    boxes.append(box)
+                case "@":
+                    robot = complex(2*x, y)
+    assert robot is not None
+    for inst in instructions:
+        positions_to_scan = {robot}
+        scanned_boxes = set[Box]()
+        try:
+            while len(positions_to_scan) > 0:
+                positions_to_scan = {s + inst for s in positions_to_scan}
+                positions_to_scan_copy = positions_to_scan.copy()
+                for scanned_pos in positions_to_scan_copy:
+                    match grid.get(scanned_pos):
+                        case Wall():
+                            raise BreakOuterLoop()
+                        case Box() as box:
+                            scanned_boxes.add(box)
+                            positions_to_scan.update([box.left, box.right])                             
+                            if inst == 1:
+                                positions_to_scan.remove(box.left)
+                            elif inst == -1:
+                                positions_to_scan.remove(box.right)
+                        case None:
+                            positions_to_scan.remove(scanned_pos)
+        except BreakOuterLoop:
+            continue
+        robot += inst
+        for box in scanned_boxes:
+            grid.pop(box.left, None)
+            grid.pop(box.right, None)
+        for box in scanned_boxes:
+            box.left += inst
+            box.right += inst
+            grid[box.left] = box          
+            grid[box.right] = box          
+
+    gps_list = []
+    for box in boxes:
+        gps_list.append(box.left.real + 100*box.left.imag)
+    print("Part 2:", int(sum(gps_list)))
 
 
 if __name__ == "__main__":
