e13c826861f6c2caf2c7a1f7477506b3745d0d49
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2025-09-22 20:37:01 +0200

    2016, day 2 part 2, Haskell

--- 2016/haskell/src/day_02.hs
+++ 2016/haskell/src/day_02.hs
@@ -1,23 +1,55 @@
-import Data.Char (intToDigit)
+import Data.Maybe (isJust, fromJust)
 problemFilename :: String
 problemFilename = "../inputs/day_02.txt"
 
 type Coord = (Int, Int)
 type CoordDelta = Coord
+type KeyPad = (Coord -> Maybe Char)
 
 main :: IO ()
 main = do
   input <- readFile problemFilename
   putStrLn $ "Problem 1: " ++ solverProb1 input
+  putStrLn $ "Problem 2: " ++ solverProb2 input
 
 parse :: String -> [String]
 parse = lines
 
-solverProb1 :: String -> String
-solverProb1 input = displayCode $ codeNumbers initCoord $ parse input
+solverProb1, solverProb2 :: String -> String
+solverProb1 = displayCode fstKeypad . codeNumbers fstKeypad fstInitCoord . parse
+solverProb2 = displayCode sndKeypad . codeNumbers sndKeypad sndInitCoord . parse
 
-initCoord :: Coord
-initCoord = (1, 1)
+fstInitCoord, sndInitCoord :: Coord
+fstInitCoord = (1, 1)
+sndInitCoord = (0, 2)
+
+fstKeypad, sndKeypad :: Coord -> Maybe Char
+fstKeypad c = case c of
+    (0, 0) -> Just '1'
+    (1, 0) -> Just '2'
+    (2, 0) -> Just '3'
+    (0, 1) -> Just '4'
+    (1, 1) -> Just '5'
+    (2, 1) -> Just '6'
+    (0, 2) -> Just '7'
+    (1, 2) -> Just '8'
+    (2, 2) -> Just '9'
+    _ -> Nothing
+sndKeypad c = case c of
+    (2, 0) -> Just '1'
+    (1, 1) -> Just '2'
+    (2, 1) -> Just '3'
+    (3, 1) -> Just '4'
+    (0, 2) -> Just '5'
+    (1, 2) -> Just '6'
+    (2, 2) -> Just '7'
+    (3, 2) -> Just '8'
+    (4, 2) -> Just '9'
+    (1, 3) -> Just 'A'
+    (2, 3) -> Just 'B'
+    (3, 3) -> Just 'C'
+    (2, 4) -> Just 'D'
+    _ -> Nothing
 
 convertInst2Delta :: Char -> CoordDelta
 convertInst2Delta c = case c of
@@ -25,26 +57,22 @@ convertInst2Delta c = case c of
   'D' -> (0, 1)
   'L' -> (-1, 0)
   'R' -> (1, 0)
-  _ -> undefined
+  other -> error $ "Invalid instruction: " ++ [other]
 
-coord2key :: Coord -> Int
-coord2key (x, y) = 1 + x + 3 * y
-
-clamp :: Int -> Int -> Int -> Int
-clamp lo hi x = max lo (min hi x)
-
-nextCoord :: Coord -> [Char] -> Coord
-nextCoord startCoord instructions =
+nextCoord :: KeyPad  -> Coord -> [Char] -> Coord
+nextCoord keyPad startCoord instructions =
   let nextStep (x, y) instruction =
         let (dx, dy) = convertInst2Delta instruction
-         in (clamp 0 2 $ x + dx, clamp 0 2 $ y + dy)
+            (cx, cy) = (x+dx, y+dy)
+            isInKeypad = keyPad (cx, cy)
+        in if isJust isInKeypad then (cx, cy) else (x, y)
    in foldl nextStep startCoord instructions
 
-codeNumbers :: Coord -> [String] -> [Coord]
-codeNumbers startCoord (inst:nextInsts) =
-    let coord =  nextCoord startCoord inst
-    in coord : codeNumbers coord nextInsts
-codeNumbers _ [] = []
+codeNumbers :: KeyPad -> Coord -> [String] -> [Coord]
+codeNumbers keyPad startCoord (inst:nextInsts) =
+    let coord =  nextCoord keyPad startCoord inst
+    in coord : codeNumbers keyPad coord nextInsts
+codeNumbers _ _ [] = []
 
-displayCode :: [Coord] -> String
-displayCode = map (intToDigit . coord2key)
+displayCode ::  KeyPad -> [Coord] -> String
+displayCode keyPad = map (fromJust . keyPad)
