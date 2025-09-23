933cbf9651d8f2981de55054ebfd6727b82a37d4
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2025-09-23 21:09:23 +0200

    2016, day 3 part 2, Haskell

--- 2016/haskell/src/day_03.hs
+++ 2016/haskell/src/day_03.hs
@@ -1,3 +1,5 @@
+import Data.List.Split (chunksOf)
+
 problemFilename :: String
 problemFilename = "../inputs/day_03.txt"
 
@@ -5,12 +7,22 @@ main :: IO ()
 main = do
   input <- readFile problemFilename
   putStrLn $ "Problem 1: " ++ solverProb1 input
+  putStrLn $ "Problem 2: " ++ solverProb2 input
 
-parse :: String -> [[Int]]
-parse input = map (map read . words) (lines input)
+isTriangle :: [Int] -> Bool
+isTriangle [a, b, c] = (a + b > c) && (a + c> b) && (b + c > a)
+isTriangle _ = False
 
-solverProb1 :: String -> String
-solverProb1 input = show $ length $ filter id $ map (\ [a, b, c] -> isTriangle a b c ) (parse input)
+parseProb1, parseProb2 :: String -> [[Int]]
+parseProb1 input = map (map read . words) (lines input)
+parseProb2 input =
+    let (x, y, z) = unzip3 $ map (toTriple . map read . words) (lines input)
+    in chunksOf 3 $ concat [x, y, z]
 
-isTriangle :: Int -> Int -> Int -> Bool
-isTriangle a b c = (a + b > c) && (a + c> b) && (b + c > a)
+solverProb1, solverProb2 :: String -> String
+solverProb1 input = show $ length $ filter id $ map isTriangle (parseProb1 input)
+solverProb2 input = show $ length $ filter id $ map isTriangle (parseProb2 input)
+
+toTriple :: [a] -> (a, a, a)
+toTriple [a, b, c] = (a, b, c)
+toTriple _ = error "Only work with list of length 3"
