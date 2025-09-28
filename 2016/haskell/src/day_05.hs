f93fb8f646800dc83e83a35eca5aabc14e79e300
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2025-09-28 08:05:59 +0200

    2016, day 5 part 2, Haskell

--- 2016/haskell/src/day_05.hs
+++ 2016/haskell/src/day_05.hs
@@ -1,7 +1,9 @@
+import qualified Crypto.Hash.MD5 as MD5
+import Data.ByteString.Base16 (encode)
 import qualified Data.ByteString.Char8 as B
-import qualified Crypto.Hash.MD5       as MD5
-import           Data.ByteString.Base16 (encode)
-import Data.List (isPrefixOf)
+import Data.List (isPrefixOf, sortOn)
+import Data.Maybe (isJust)
+import Text.Read (readMaybe)
 
 problemFilename :: String
 problemFilename = "../inputs/day_05.txt"
@@ -10,25 +12,38 @@ main :: IO ()
 main = do
   input <- readFile problemFilename
   putStrLn $ "Problem 1: " ++ solverProb1 input
-  -- putStrLn $ "Problem 2: " ++ solverProb2 input
+  putStrLn $ "Problem 2: " ++ solverProb2 input
 
-solverProb1 :: String -> String
-solverProb1 = computePassword [] 0
+solverProb1, solverProb2 :: String -> String
+solverProb1 = computePasswordProb1 [] 0
+solverProb2 = computePasswordProb2 [] 0
 
-computePassword :: [Char] -> Int -> String -> String
-computePassword password index doorId =
+computePasswordProb1 :: [Char] -> Int -> String -> String
+computePasswordProb1 password index doorId =
   if length password == 8
-  then reverse password
-  else let (newChar, newIndex) = nextCodeChar index doorId
-       in computePassword (newChar : password) newIndex doorId
+    then reverse password
+    else
+      let (newIndex, newChar, _) = nextCodeChar index doorId
+       in computePasswordProb1 (newChar : password) newIndex doorId
 
-nextCodeChar ::  Int -> String -> (Char, Int)
+computePasswordProb2 :: [(Int, Char)] -> Int -> String -> String
+computePasswordProb2 password index doorId =
+  if length password == 8
+    then snd <$> sortOn fst password
+    else
+      let (newIndex, charPosStr, newChar) = nextCodeChar index doorId
+          charPos = readMaybe [charPosStr] :: Maybe Int
+          newPassword = case charPos of
+            Nothing -> password
+            Just pos -> if isJust (lookup pos password) || pos > 7 then password else (pos, newChar) : password
+       in computePasswordProb2 newPassword newIndex doorId
+
+nextCodeChar :: Int -> String -> (Int, Char, Char)
 nextCodeChar index doorId =
   let hash = computeMD5 $ doorId ++ show index
-  in if "00000" `isPrefixOf` hash
-     then (hash !! 5, index +1)
-     else nextCodeChar (index+1) doorId
+   in if "00000" `isPrefixOf` hash
+        then (index + 1, hash !! 5, hash !! 6)
+        else nextCodeChar (index + 1) doorId
 
 computeMD5 :: String -> String
 computeMD5 = B.unpack . encode . MD5.hash . B.pack
-
