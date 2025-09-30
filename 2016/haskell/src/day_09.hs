afe5014175c85d9631c20697d2a6612b4e276450
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2025-09-30 20:21:52 +0200

    2016, day 9 part 1, Haskell

---
+++ 2016/haskell/src/day_09.hs
@@ -0,0 +1,30 @@
+problemFilename :: String
+problemFilename = "../inputs/day_09.txt"
+
+main :: IO ()
+main = do
+  input <- readFile problemFilename
+  putStrLn $ "Problem 1: " ++ solverProb1 input
+
+solverProb1 :: String -> String
+solverProb1 = show . length . expandCompressed
+
+expandCompressed :: String -> String
+expandCompressed compressed =
+  let (_, expanded) = continueExpansion (compressed, [])
+   in expanded
+
+continueExpansion :: (String, String) -> (String, String)
+continueExpansion ([], expanded) = ([], expanded)
+continueExpansion (compressed, expanded) = case head compressed of
+  '(' -> let (left, right) = break (== ')') $ tail compressed
+             (size, times) = parseExpansionCommand left
+             chunk = take size (drop 1 right)
+             nextCompressed = drop (1+size) right
+          in continueExpansion (nextCompressed, expanded ++ concat (replicate times chunk))
+  c -> continueExpansion (tail compressed, expanded ++ [c])
+
+parseExpansionCommand :: String -> (Int, Int)
+parseExpansionCommand command =
+  let (left, right) = break (=='x') command
+   in (read left, read $ tail right)
