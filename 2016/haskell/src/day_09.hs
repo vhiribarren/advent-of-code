2a2dba060f744a6d036afd57f5b9edb81f77605b
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2025-09-30 22:28:30 +0200

    refactoring

--- 2016/haskell/src/day_09.hs
+++ 2016/haskell/src/day_09.hs
@@ -8,39 +8,32 @@ main = do
   putStrLn $ "Problem 2: " ++ solverProb2 input
 
 solverProb1, solverProb2 :: String -> String
-solverProb1 = show . length . expandCompressed
-solverProb2 = show . fullExpandedLength
+solverProb1 = show . fst . continueSimpleExpansion 0
+solverProb2 = show . fst . continueFullExpansion 0
+
+continueSimpleExpansion :: Int -> String -> (Int, String)
+continueSimpleExpansion len [] = (len, [])
+continueSimpleExpansion len compressed = case head compressed of
+  '(' ->
+    let (left, right) = break (== ')') $ tail compressed
+        (size, times) = parseExpansionCommand left
+        nextCompressed = drop (1 + size) right
+     in continueSimpleExpansion (len + times * size) nextCompressed
+  _ -> continueSimpleExpansion (len + 1) (tail compressed)
+
+continueFullExpansion :: Int -> String -> (Int, String)
+continueFullExpansion len [] = (len, [])
+continueFullExpansion len compressed = case head compressed of
+  '(' ->
+    let (left, right) = break (== ')') $ tail compressed
+        (size, times) = parseExpansionCommand left
+        chunk = take size (drop 1 right)
+        nextCompressed = drop (1 + size) right
+        nextLen = times * fst (continueFullExpansion 0 chunk)
+     in continueFullExpansion (len + nextLen) nextCompressed
+  _ -> continueFullExpansion (len + 1) (tail compressed)
 
 parseExpansionCommand :: String -> (Int, Int)
 parseExpansionCommand command =
   let (left, right) = break (== 'x') command
    in (read left, read $ tail right)
-
-expandCompressed :: String -> String
-expandCompressed compressed = snd $ continueExpansion (compressed, [])
-
-continueExpansion :: (String, String) -> (String, String)
-continueExpansion ([], expanded) = ([], expanded)
-continueExpansion (compressed, expanded) = case head compressed of
-  '(' ->
-    let (left, right) = break (== ')') $ tail compressed
-        (size, times) = parseExpansionCommand left
-        chunk = take size (drop 1 right)
-        nextCompressed = drop (1 + size) right
-     in continueExpansion (nextCompressed, expanded ++ concat (replicate times chunk))
-  c -> continueExpansion (tail compressed, expanded ++ [c])
-
-fullExpandedLength :: String -> Int
-fullExpandedLength input = snd $ continueFullExpansion (input, 0)
-
-continueFullExpansion :: (String, Int) -> (String, Int)
-continueFullExpansion ([], len) = ([], len)
-continueFullExpansion (compressed, len) = case head compressed of
-  '(' ->
-    let (left, right) = break (== ')') $ tail compressed
-        (size, times) = parseExpansionCommand left
-        chunk = take size (drop 1 right)
-        nextCompressed = drop (1 + size) right
-        nextLen = times * snd (continueFullExpansion (chunk, 0))
-     in continueFullExpansion (nextCompressed, len + nextLen)
-  _ -> continueFullExpansion (tail compressed, len + 1)
