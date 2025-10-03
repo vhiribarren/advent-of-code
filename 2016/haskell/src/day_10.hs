16b51aa5f2b0308bd348074547489e9f8292fa82
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2025-10-03 21:36:58 +0200

    2016, day 10 part 2, Haskell

--- 2016/haskell/src/day_10.hs
+++ 2016/haskell/src/day_10.hs
@@ -18,7 +18,8 @@ data Bot = Bot
   }
   deriving (Show)
 type Bots = IntMap Bot
-type Outputs = IntMap [ElementId]
+type Outputs = IntMap ElementId
+type Containers = (Bots, Outputs)
 
 targetComboChip :: [ElementId]
 targetComboChip = [61, 17]
@@ -27,17 +28,18 @@ main :: IO ()
 main = do
   input <- readFile problemFilename
   putStrLn $ "Problem 1: " ++ solverProb1 input
--- putStrLn $ "Problem 2: " ++ solverProb2 input
+  putStrLn $ "Problem 2: " ++ solverProb2 input
 
 solverProb1, solverProb2 :: String -> String
 solverProb1 = show . botId . runSimulationUntil targetComboChip . parse
-solverProb2 = undefined
+solverProb2 = show . multChip . snd . runFullSimulation . parse
+  where multChip o = o IntMap.!0 * o IntMap.!1 * o IntMap.!2
 
-parse :: String -> Bots
+parse :: String -> Containers
 parse input =
   let (graph, initVal) = partition ("bot" `isPrefixOf`) $ lines input
       bots = foldl buildBotGraph IntMap.empty (map words graph)
-   in foldl injectInitVal bots (map words initVal)
+   in (foldl injectInitVal bots (map words initVal), IntMap.empty)
 
 buildBotGraph :: Bots -> [String] -> Bots
 buildBotGraph bots w =
@@ -58,30 +60,37 @@ passValToBot val = IntMap.update (\b -> Just (b {botElements = val : botElements
 parseTarget :: String -> String -> Target
 parseTarget target id' = (if target == "bot" then BotId else OutputId) (read id')
 
-runSimulationUntil :: [ElementId] -> Bots -> Bot
-runSimulationUntil comboChip bots =
-  let bot = traceShowId $ findBotWith2Chips bots
+runSimulationUntil :: [ElementId] -> Containers -> Bot
+runSimulationUntil comboChip containers@(bots, _) =
+  let bot = fromJust $ traceShowId $ findBotWith2Chips bots
    in if isBotFound comboChip bot
       then bot
-      else runSimulationUntil comboChip $ transferBotElements bot bots
+      else runSimulationUntil comboChip $ transferBotElements bot containers
+
+runFullSimulation :: Containers -> Containers
+runFullSimulation containers@(bots, _) =
+  let bot = traceShowId $ findBotWith2Chips bots
+   in case bot of
+        Nothing -> containers
+        Just b -> runFullSimulation $ transferBotElements b containers
 
 isBotFound :: [ElementId] -> Bot -> Bool
 isBotFound comboChip bot = all (`elem` botElements bot) comboChip
 
-findBotWith2Chips :: Bots -> Bot
-findBotWith2Chips = snd . fromJust . find (\(_, v) -> length (botElements v) == 2) . IntMap.toList
+findBotWith2Chips :: Bots -> Maybe Bot
+findBotWith2Chips = fmap snd . find (\(_, v) -> length (botElements v) == 2) . IntMap.toList
 
-transferBotElements :: Bot -> Bots -> Bots
-transferBotElements bot bots =
+transferBotElements :: Bot -> Containers -> Containers
+transferBotElements bot (bots, outputs) =
   let highVal = maximum $ botElements bot
       lowVal = minimum $ botElements bot
       lowTarget = low bot
       highTarget = high bot
       bots' = IntMap.update (\_ -> Just $ bot {botElements = []}) (botId bot) bots
-      bots'' = case lowTarget of
-        BotId i -> passValToBot lowVal i bots'
-        _ -> bots'
-      bots''' = case highTarget of
-        BotId i -> passValToBot highVal i bots''
-        _ -> bots''
-   in bots'''
+      (bots'', outputs'') = case lowTarget of
+        BotId i -> (passValToBot lowVal i bots', outputs)
+        OutputId i -> (bots', IntMap.insert i lowVal outputs)
+      result = case highTarget of
+        BotId i -> (passValToBot highVal i bots'', outputs'')
+        OutputId i -> (bots'', IntMap.insert i highVal outputs'')
+   in result
