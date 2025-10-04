bd27aa36f969266f251905e30f9bcdd239bdfc3a
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2025-10-04 07:04:27 +0200

    playing with Haskell types

--- 2016/haskell/src/day_10.hs
+++ 2016/haskell/src/day_10.hs
@@ -1,5 +1,5 @@
-import Data.IntMap (IntMap)
-import qualified Data.IntMap.Strict as IntMap
+import Data.Map (Map)
+import qualified Data.Map as Map
 import Data.List (isPrefixOf, partition, find)
 import Data.Maybe (fromJust)
 import Debug.Trace (traceShowId)
@@ -7,18 +7,18 @@ import Debug.Trace (traceShowId)
 problemFilename :: String
 problemFilename = "../inputs/day_10.txt"
 
-type BotId = Int
-type OutputId = Int
+newtype BotId = BotId Int deriving (Show, Ord, Eq)
+newtype OutputId = OutputId Int deriving (Show, Ord, Eq)
 type ElementId = Int
-data Target = OutputId OutputId | BotId BotId deriving (Show)
+data Target = OutputTarget OutputId | BotTarget BotId deriving (Show)
 data Bot = Bot
   { botId :: BotId,
     botElements :: [ElementId],
     low, high :: Target
   }
   deriving (Show)
-type Bots = IntMap Bot
-type Outputs = IntMap ElementId
+type Bots = Map BotId Bot
+type Outputs = Map OutputId ElementId
 type Containers = (Bots, Outputs)
 
 targetComboChip :: [ElementId]
@@ -33,32 +33,35 @@ main = do
 solverProb1, solverProb2 :: String -> String
 solverProb1 = show . botId . runSimulationUntil targetComboChip . parse
 solverProb2 = show . multChip . snd . runFullSimulation . parse
-  where multChip o = o IntMap.!0 * o IntMap.!1 * o IntMap.!2
+  where multChip o = product $ (o Map.!) <$> [OutputId 0, OutputId 1, OutputId 2]
 
 parse :: String -> Containers
 parse input =
   let (graph, initVal) = partition ("bot" `isPrefixOf`) $ lines input
-      bots = foldl buildBotGraph IntMap.empty (map words graph)
-   in (foldl injectInitVal bots (map words initVal), IntMap.empty)
+      bots = foldl buildBotGraph Map.empty (map words graph)
+   in (foldl injectInitVal bots (map words initVal), Map.empty)
 
 buildBotGraph :: Bots -> [String] -> Bots
 buildBotGraph bots w =
-  let botId' = read $ w !! 1
+  let botId' = BotId $ read $ w !! 1
       lowTarget = parseTarget (w !! 5) (w !! 6)
       highTarget = parseTarget (w !! 10) (w !! 11)
-   in IntMap.insert botId' (Bot botId' [] lowTarget highTarget) bots
+   in Map.insert botId' (Bot botId' [] lowTarget highTarget) bots
 
 injectInitVal :: Bots -> [String] -> Bots
 injectInitVal bots w =
   let val = read $ w !! 1
-      botId' = read $ w !! 5
+      botId' = BotId $ read $ w !! 5
    in passValToBot val botId' bots
 
 passValToBot :: ElementId -> BotId -> Bots -> Bots
-passValToBot val = IntMap.update (\b -> Just (b {botElements = val : botElements b}))
+passValToBot val = Map.update (\b -> Just (b {botElements = val : botElements b}))
 
 parseTarget :: String -> String -> Target
-parseTarget target id' = (if target == "bot" then BotId else OutputId) (read id')
+parseTarget target id' =
+  if target == "bot"
+  then BotTarget $ BotId (read id')
+  else OutputTarget $ OutputId (read id')
 
 runSimulationUntil :: [ElementId] -> Containers -> Bot
 runSimulationUntil comboChip containers@(bots, _) =
@@ -78,7 +81,7 @@ isBotFound :: [ElementId] -> Bot -> Bool
 isBotFound comboChip bot = all (`elem` botElements bot) comboChip
 
 findBotWith2Chips :: Bots -> Maybe Bot
-findBotWith2Chips = fmap snd . find (\(_, v) -> length (botElements v) == 2) . IntMap.toList
+findBotWith2Chips = fmap snd . find (\(_, v) -> length (botElements v) == 2) . Map.toList
 
 transferBotElements :: Bot -> Containers -> Containers
 transferBotElements bot (bots, outputs) =
@@ -86,11 +89,11 @@ transferBotElements bot (bots, outputs) =
       lowVal = minimum $ botElements bot
       lowTarget = low bot
       highTarget = high bot
-      bots' = IntMap.update (\_ -> Just $ bot {botElements = []}) (botId bot) bots
+      bots' = Map.update (\_ -> Just $ bot {botElements = []}) (botId bot) bots
       (bots'', outputs'') = case lowTarget of
-        BotId i -> (passValToBot lowVal i bots', outputs)
-        OutputId i -> (bots', IntMap.insert i lowVal outputs)
+        BotTarget i -> (passValToBot lowVal i bots', outputs)
+        OutputTarget i -> (bots', Map.insert i lowVal outputs)
       result = case highTarget of
-        BotId i -> (passValToBot highVal i bots'', outputs'')
-        OutputId i -> (bots'', IntMap.insert i highVal outputs'')
+        BotTarget i -> (passValToBot highVal i bots'', outputs'')
+        OutputTarget i -> (bots'', Map.insert i highVal outputs'')
    in result
