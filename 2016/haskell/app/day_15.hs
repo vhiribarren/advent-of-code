371593d7d4467512503b6e43c2cd054c1b173477
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2025-10-15 19:15:54 +0200

    2016, day 15 part 2, Haskell

--- 2016/haskell/app/day_15.hs
+++ 2016/haskell/app/day_15.hs
@@ -13,15 +13,21 @@ data Disc = Disc {
   current_pos :: Int
 } deriving (Show)
 
+specialDisc :: Disc
+specialDisc = Disc 0 11 0 0 -- Setting ID as in problem, but we then change it to adapt to algorithm
+
 main :: IO ()
 main = do
   input <- readFile problemFilename
   putStrLn $ "Problem 1: " ++ solverProb1 input
-  -- putStrLn $ "Problem 2: " ++ solverProb2 input
+  putStrLn $ "Problem 2: " ++ solverProb2 input
 
 solverProb1, solverProb2 :: String -> String
 solverProb1 = show . searchStartTime . inputParser
-solverProb2 = undefined
+solverProb2 input = show $ searchStartTime (initDiscs ++ [sDisc])
+  where initDiscs = inputParser input
+        sDisc = specialDisc { discId = discId (last initDiscs) + 1 } -- Changing the ID ; solution is quite tied to my input instance
+                                                                     -- where disc ID are starting with 1 and are incremented
 
 inputParser :: String -> [Disc]
 inputParser input = map parseLine (lines input)
