e04a78cf8d51cfd7acfa69fb0e0b650e5e316cd0
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2025-12-08 19:18:25 +0100

    a bit of simplification in the code

--- 2025/python/day_08.py
+++ 2025/python/day_08.py
@@ -16,21 +16,13 @@ def dist_square(left, right):
 
 def part_1(input_data: str):
     junctions = [tuple(map(int, line.split(","))) for line in input_data.splitlines()]
-    pairs = []
-    for (l, r) in combinations(junctions, 2):
-        pairs.append((dist_square(l, r), l, r))
+    pairs = [(dist_square(l, r), l, r) for (l, r) in combinations(junctions, 2)]
     pairs.sort(key=lambda p: p[0])
     groups = []
     for (_, l, r) in pairs[:MAX_JUNCTIONS]:
-        target_idx = [idx for idx, group in enumerate(groups) if l in group or r in group]
-        if len(target_idx) == 0:
-            groups.append({l, r})
-        elif len(target_idx) == 1:
-            groups[target_idx[0]] |= {l, r}
-        else:
-            fst_grp = groups.pop(target_idx[1]) # pop in reverse order!!
-            snd_grp = groups.pop(target_idx[0])
-            groups.append(fst_grp | snd_grp | {l, r})
+        target_idx = sorted([idx for idx, group in enumerate(groups) if l in group or r in group], reverse=True)
+        found_groups = [groups.pop(idx) for idx in target_idx]
+        groups.append({l, r}.union(*found_groups))
     box_sizes = sorted([len(g) for g in groups], reverse=True)
     result = prod(box_sizes[:MAX_CIRCUITS])
     print("Part 1:", result)
@@ -38,21 +30,13 @@ def part_1(input_data: str):
 
 def part_2(input_data: str):
     junctions = [tuple(map(int, line.split(","))) for line in input_data.splitlines()]
-    pairs = []
-    for (l, r) in combinations(junctions, 2):
-        pairs.append((dist_square(l, r), l, r))
+    pairs = [(dist_square(l, r), l, r) for (l, r) in combinations(junctions, 2)]
     pairs.sort(key=lambda p: p[0])
     groups = []
     for (_, l, r) in pairs:
-        target_idx = [idx for idx, group in enumerate(groups) if l in group or r in group]
-        if len(target_idx) == 0:
-            groups.append({l, r})
-        elif len(target_idx) == 1:
-            groups[target_idx[0]] |= {l, r}
-        else:
-            fst_grp = groups.pop(target_idx[1]) # pop in reverse order!!
-            snd_grp = groups.pop(target_idx[0])
-            groups.append(fst_grp | snd_grp | {l, r})
+        target_idx = sorted([idx for idx, group in enumerate(groups) if l in group or r in group], reverse=True)
+        found_groups = [groups.pop(idx) for idx in target_idx]
+        groups.append({l, r}.union(*found_groups))
         if len(groups[0]) == len(junctions):
             break
     result = l[0] * r[0]
