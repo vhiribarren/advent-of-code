1d546718920d6681adbc2176b88bf40e30a5ddab
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2026-01-08 22:48:48 +0100

    2015, day 14, part 1, Python

---
+++ 2015/python/day_14.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+from dataclasses import dataclass
+
+INPUT_FILEPATH = (p := Path(__file__)).parent/".."/"inputs"/f"{p.stem}.txt"
+
+TARGET_TIME = 2503
+
+@dataclass
+class Reindeer:
+    speed: int
+    fly_time: int
+    rest_time: int
+
+def parse(input_data: str) -> list[Reindeer]:
+    return [
+        Reindeer(int(w[3]), int(w[6]), int(w[13]))
+        for w in (l.split() for l in input_data.splitlines())
+    ]
+
+def reindeer_dist(r: Reindeer, time: int) -> int:
+    time_chunk = r.fly_time + r.rest_time
+    chunk, rest = divmod(time, time_chunk)
+    return chunk*r.speed*r.fly_time + r.speed*min(rest, r.fly_time)
+
+def part_1(input_data: str):
+    reindeers = parse(input_data)
+    distances = [reindeer_dist(r, TARGET_TIME) for r in reindeers]
+    result = max(distances)
+    print("Part 1:", result)
+
+
+def part_2(input_data: str):
+    result = None
+    print("Part 2:", result)
+
+
+if __name__ == "__main__":
+    file_data = INPUT_FILEPATH.read_text()
+    part_1(file_data)
+    part_2(file_data)
