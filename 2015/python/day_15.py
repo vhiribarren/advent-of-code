9dc4d146edf3e6a68f1445797dcccf152af9e3d0
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2026-01-10 22:25:03 +0100

    2015, day 15, part 2, Python

--- 2015/python/day_15.py
+++ 2015/python/day_15.py
@@ -4,12 +4,14 @@ from pathlib import Path
 from math import prod
 
 INPUT_FILEPATH = (p := Path(__file__)).parent/".."/"inputs"/f"{p.stem}.txt"
+TOTAL_CALORIES = 500
+TOTAL_SPOON = 100
 
 type Ingredient = list[int]
 
 def parser(input_data: str) -> list[Ingredient]:
     return [
-        list(map(int, [cap[:-1], dur[:-1], flav[:-1], tex[:-1]]))
+        list(map(int, [cap[:-1], dur[:-1], flav[:-1], tex[:-1], cal]))
         for _, _, cap, _, dur, _, flav, _, tex, _, cal
         in (line.split() for line in input_data.splitlines())
     ]
@@ -22,20 +24,23 @@ def compositions(total: int, n: int) -> tuple[int, ...]:
             for rest in compositions(total - x, n - 1):
                 yield (x,) + rest
 
-def part_1(input_data: str):
+def solver(input_data: str, with_calories: bool):
     ingredients = parser(input_data)
-    prop_num = len(ingredients[0])
+    prop_num = len(ingredients[0])-1
     scores = []
-    for spoons in compositions(100, prop_num):
+    for spoons in compositions(TOTAL_SPOON, prop_num):
         quantities = [[spoon*p for p in props] for (spoon, props) in zip(spoons, ingredients)]
-        scores.append(prod([max(v, 0) for v in map(sum, zip(*quantities))]))
-    result = max(scores)
-    print("Part 1:", result)
+        sum_quantities = [max(v, 0) for v in map(sum, zip(*quantities))]
+        if with_calories and sum_quantities[-1] != 500:
+            continue
+        scores.append(prod(sum_quantities[:-1]))
+    return max(scores)
 
+def part_1(input_data: str):
+    print("Part 1:", solver(input_data, False))
 
 def part_2(input_data: str):
-    result = None
-    print("Part 2:", result)
+    print("Part 1:", solver(input_data, True))
 
 
 if __name__ == "__main__":
