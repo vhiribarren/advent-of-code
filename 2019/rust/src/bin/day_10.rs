5affe57b11a263e9afa1f78870f1cb7548d7ec33
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2026-08-09 17:07:46 +0200

    2019, day 10 part 2, Rust

--- 2019/rust/src/bin/day_10.rs
+++ 2019/rust/src/bin/day_10.rs
@@ -1,7 +1,13 @@
 use std::{
-    collections::{HashMap, HashSet}, error::Error, fs, path::{Path, PathBuf}, sync::LazyLock, unimplemented,
+    collections::{HashMap, HashSet},
+    error::Error,
+    f64::consts::PI,
+    fs,
+    path::{Path, PathBuf},
+    sync::LazyLock,
 };
 
+use itertools::Itertools;
 use num_integer::gcd;
 
 const INPUT_FILENAME: &str = "day_10.txt";
@@ -21,34 +27,28 @@ fn main() -> Result<(), Box<dyn Error>> {
 
 type NormalizedVector = (isize, isize);
 type Count = usize;
+type Coords = (isize, isize);
 
 fn problem_1(input: &str) -> Result<String, Box<dyn Error>> {
     let map = parse_input(input);
-    let mut results = HashMap::<Coords, Count>::new();
-    for candidate in &map {
-        let mut nvects = HashSet::<NormalizedVector>::new();
-        for asteroid in &map {
-            if candidate == asteroid {
-                continue;
-            }
-            let v = (asteroid.0 - candidate.0, asteroid.1 - candidate.1);
-            let gcd = gcd(v.0.abs(), v.1.abs());
-            nvects.insert((
-                v.0 / gcd,
-                v.1 / gcd
-            ));
-        }
-        results.insert(*candidate, nvects.len());
-    }
-    Ok(results.values().max().unwrap().to_string())
+    let (_, count, _) = get_best_station(&map);
+    Ok(count.to_string())
 }
 
 fn problem_2(input: &str) -> Result<String, Box<dyn Error>> {
     let map = parse_input(input);
-    unimplemented!()
+    let (_, _, rays) = get_best_station(&map);
+    let result = rays
+        .into_iter()
+        .map(|(k, v)| ((k.0 as f64).atan2(-(k.1 as f64)).rem_euclid(2.0 * PI), v))
+        .sorted_by(|l, r| l.0.total_cmp(&r.0))
+        .map(|(_, v)| v.into_iter().sorted_by_key(|v| v.0 * v.0 + v.1 * v.1))
+        .cycle()
+        .filter_map(|mut r| r.next())
+        .nth(199)
+        .unwrap();
+    Ok((result.0 * 100 + result.1).to_string())
 }
-
-type Coords = (isize, isize);
 
 fn parse_input(input: &str) -> HashSet<Coords> {
     let mut map = HashSet::new();
@@ -60,4 +60,34 @@ fn parse_input(input: &str) -> HashSet<Coords> {
         }
     }
     map
+}
+
+fn get_best_station(
+    map: &HashSet<Coords>,
+) -> (Coords, Count, HashMap<NormalizedVector, Vec<Coords>>) {
+    aligned_asteroids(map)
+        .into_iter()
+        .map(|(candidate, v)| (candidate, v.keys().len(), v))
+        .max_by_key(|(_, l, _)| *l)
+        .unwrap()
+}
+
+fn aligned_asteroids(
+    map: &HashSet<Coords>,
+) -> HashMap<Coords, HashMap<NormalizedVector, Vec<Coords>>> {
+    let mut results = HashMap::new();
+    for candidate in map {
+        let mut same_ray = HashMap::<NormalizedVector, Vec<Coords>>::new();
+        for asteroid in map {
+            if candidate == asteroid {
+                continue;
+            }
+            let v = (asteroid.0 - candidate.0, asteroid.1 - candidate.1);
+            let gcd = gcd(v.0.abs(), v.1.abs());
+            let normalized_v = (v.0 / gcd, v.1 / gcd);
+            same_ray.entry(normalized_v).or_default().push(*asteroid);
+        }
+        results.insert(*candidate, same_ray);
+    }
+    results
 }
